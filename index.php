a470baa0245ff3230b6a8114c1b644bc02ef05ce
Sharkpb8 <sharkpb8@gmail.com>
2024-05-21 15:57:33 +0200

    home button moved to ul

--- index.php
+++ index.php
@@ -11,12 +11,14 @@
 <body>
     <nav class="navbar navbar-expand-lg" style="background-color: #423c3c;">
         <div class="container-fluid">
-          <a class="navbar-brand" href="index.php" style="color:white">Home</a>
           <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
           </button>
           <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav me-auto mb-2 mb-lg-0 nav-underline">
+            <li class="nav-item">
+              <a class="nav-link active" href="#" style="color:white;">Home</a>
+            </li>
             <?php
             session_start();
               if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true){
