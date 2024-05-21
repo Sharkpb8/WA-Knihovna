56fbf4745d39110cbed673fa09507181d36c78e2
Sharkpb8 <sharkpb8@gmail.com>
2024-05-21 16:24:52 +0200

    footer and katalog page added

--- index.php
+++ index.php
@@ -3,19 +3,20 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Home</title>
+    <link rel="icon" type="image/x-icon" href="/img/favicon.png">
     <link rel="stylesheet" href="style.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </head>
 <body>
-    <nav class="navbar navbar-expand-lg" style="background-color: #423c3c;">
+    <nav class="navbar navbar-expand-lg" style="background-color: #333;">
         <div class="container-fluid">
           <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
           </button>
           <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav me-auto mb-2 mb-lg-0 nav-underline">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0 nav-underline ">
             <li class="nav-item">
               <a class="nav-link active" href="#" style="color:white;">Home</a>
             </li>
@@ -26,7 +27,7 @@
                 echo  '<a class="nav-link" aria-current="page" href="logout.php" style="color:white">Log out</a>';
                 echo '</li>';
                 echo '<li class="nav-item">';
-                echo '<a class="nav-link" href="data.php" style="color:white">Katalog</a>';
+                echo '<a class="nav-link" href="katalog_page.php" style="color:white">Katalog</a>';
                 echo ' </li>';
                 echo '<li class="nav-item">';
                 echo '<a class="nav-link" href="threads_page.php" style="color:white">Moje půjčky</a>';
@@ -49,7 +50,7 @@
       </div> -->
       <div class="outer-container">
         <div class="inner-container">
-            <h1>Knihovna U Zlaté Hvězdy</h1>
+            <h1>Městská knihovna Praha</h1>
             <p>Otevírací Doba</p>
             <ul>
               <li>Po-Pá: 9:00 - 20:00</li>
@@ -64,5 +65,8 @@
             </ul>
         </div>
     </div>
+    <footer>
+      <p>Made By Adam Hlaváčik</p>
+    </footer>
 </body>
 </html>
--- register_page.php
+++ register_page.php
@@ -3,13 +3,13 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Register</title>
     <link rel="stylesheet" href="style.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </head>
 <body>
-<nav class="navbar navbar-expand-lg " style="background-color: #423c3c;">
+<nav class="navbar navbar-expand-lg " style="background-color: #333;">
         <div class="container-fluid">
           <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
@@ -52,6 +52,8 @@
     </form>
     </div>
     </div>
-
+    <footer>
+      <p>Made By Adam Hlaváčik</p>
+    </footer>
 </body>
 </html>
