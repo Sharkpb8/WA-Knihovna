c21bff4538c6c335e40554e30e165ac0e0bef6d9
Sharkpb8 <sharkpb8@gmail.com>
2024-05-21 15:01:16 +0200

    background with text white background added

--- index.php
+++ index.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="style.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </head>
@@ -39,9 +40,15 @@
           </div>
         </div>
       </nav>
-
+      <!-- <div class="textcenter">
       <p>Vítejte na naší webové stránce!</p>
       <p>Jsme váš průvodce do světa inspirace, objevování a zábavy.</p>
       <p>Nechte se unést naší bohatou paletou obsahu, který osloví vaše smysly, povzbudí vaši kreativitu a posune vaše myšlenky na nové úrovně.</p>
+      </div> -->
+      <div class="outer-container">
+        <div class="inner-container">
+            <p>Your text goes here. This text block has a white background and occupies 60% of the width, with 20% space on either side.</p>
+        </div>
+    </div>
 </body>
 </html>
