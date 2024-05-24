4f4a2cb547c120c893a8ec0885f5f01a79b66881
Sharkpb8 <sharkpb8@gmail.com>
2024-05-24 15:11:52 +0200

    comments removed

--- addauthor.php
+++ addauthor.php
@@ -3,10 +3,8 @@ require_once "./classes/DBC.php";
 require_once "./classes/User.php";
 session_start();
 
-// Vložení nového příspěvku do databáze
 $query = DBC::getConnection()->query("call addauthor('" . $_POST["authorname"] . "','" . $_POST["authorsurename"] . "');");
 
-// Přesměrování na domovskou stránku nebo jinou relevantní stránku
 header('Location: adding_page.php');
 exit();
 ?>
--- addbook.php
+++ addbook.php
@@ -3,10 +3,8 @@ require_once "./classes/DBC.php";
 require_once "./classes/User.php";
 session_start();
 
-// Vložení nového příspěvku do databáze
 $query = DBC::getConnection()->query("call addbook('" . $_POST["genre"] . "','" . $_POST["authorname"] . "','" . $_POST["authorsurename"] . "','" . $_POST["Name"] . "','" . $_POST["releasedate"] . "');");
 
-// Přesměrování na domovskou stránku nebo jinou relevantní stránku
 header('Location: addbook_page.php');
 exit();
 ?>
